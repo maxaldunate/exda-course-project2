e5121e33a4a5ff87ea581e9ff26089864555de2c
Max Aldunate <maxaldunate@gmail.com>
2017-01-17 20:20:52 +0100

    first plot trying

---
+++ plot1.R
@@ -0,0 +1,24 @@
+#Week 4. Peer-graded Assignment: Course Project 2
+
+#Plot 1
+#Using the base plotting system, make a plot showing the total PM2.5 emission 
+#from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+if(!exists("NEI")){
+  NEI <- readRDS("data/summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("data/Source_Classification_Code.rds")
+}
+
+data1 <- aggregate(Emissions ~ year, NEI, sum)
+
+png("plot1.png", width = 480, height = 480, units = "px")
+
+barplot((data1$Emissions)/10^6,
+        names.arg = data1$year,
+        main="Total PM2.5 Emissions From All US Sources",
+        xlab = "Year", ylab="PM2.5 Emissions (10^6 Tons)"
+)
+
+dev.off()
--- script.R
+++
@@ -1,4 +0,0 @@
-#Week 4. Peer-graded Assignment: Course Project 2
-
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
